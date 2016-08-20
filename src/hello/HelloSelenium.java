6147a3e43c285d9bd076572344f647ca83c71176
riz <riz_aziz2002@mail.ru>
2016-08-20 13:45:13 -0400

    Selenium testing

--- src/hello/HelloSelenium.java
+++ src/hello/HelloSelenium.java
@@ -1,8 +1,28 @@
 package hello;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
 public class HelloSelenium {
 	public static void main(String[] args){
+		System.setProperty("webdriver.chrome.driver","F:\\SeleniumDrivers\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","F:\\SeleniumDrivers\\geckodriver.exe");
+		System.setProperty("webdriver.ie.driver","F:\\SeleniumDrivers\\IEDriverServer.exe");
+		System.setProperty("webdriver.opera.driver","F:\\SeleniumDrivers\\operadriver.exe");
 		System.out.println("Hello Selenium");
+		//WebDriver driver=new FirefoxDriver();
+		//WebDriver driver=new ChromeDriver();
+		//WebDriver driver=new InternetExplorerDriver();
+		WebDriver driver=new OperaDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.MICROSECONDS);
+		driver.navigate().to("http://amazon.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Laptop", Keys.ENTER);
+		
 	}
-
 }
